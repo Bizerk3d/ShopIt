ee2e6eb82bf62702ad4009f4794fe3f14fca108c
Brennan Reed <brtreed3@gmail.com>
2023-07-09 23:05:14 -0400

    Implement product routes/controllers

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import productRoutes from './routes/productRoutes.js';
 const port = 5000;
 
 // Connect to MongoDB
@@ -14,15 +15,10 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-// all products
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+// direct any requests with 1st param through ProductRoutes 
+app.use('/api/products', productRoutes);
 
-// single product
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
