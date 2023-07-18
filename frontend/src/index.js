562e65c84517919a809b235a788a370cb12d4d25
Brennan Reed <brtreed3@gmail.com>
2023-07-18 13:18:10 -0400

    Adding admin functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,10 +20,13 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import OrderListScreen from './screens/admin/OrderListScreen';
+import AdminRoute from './components/AdminRoute';
 import PrivateRoute from './components/PrivateRoute';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -40,6 +43,12 @@ const router = createBrowserRouter(
                 <Route path='/payment' element={<PaymentScreen />} />
                 <Route path='/placeorder' element={<PlaceOrderScreen />} />
                 <Route path='/order/:id' element={<OrderScreen />} />
+                <Route path='/profile' element={<ProfileScreen />} />
+            </Route>
+
+            {/* Admin Users */}
+            <Route path='' element={<AdminRoute />}>
+                <Route path='/admin/orderlist' element={<OrderListScreen />} />
             </Route>
         </Route>
     )
