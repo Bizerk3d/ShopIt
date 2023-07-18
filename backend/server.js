c05a77e0188a2443fdb7232f1aac18b9cc727d29
Brennan Reed <brtreed3@gmail.com>
2023-07-17 22:48:03 -0400

    Finish Checkout functionality

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import cookieParser from 'cookie-parser';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
-const port = 5000;
+import orderRoutes from './routes/orderRoutes.js';
+
+const port = process.env.PORT || 5000;
 
 // Connect to MongoDB
 connectDB();
@@ -27,6 +29,9 @@ app.get('/', (req, res) => {
 // direct any requests with 1st param through ProductRoutes 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+
+app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID }));
 
 app.use(notFound);
 app.use(errorHandler);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,9 @@ import {
   createBrowserRouter,
   createRoutesFromElements, 
   Route,
-  RouteProvider,
   RouterProvider
 } from 'react-router-dom';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import store from './store';
 import { Provider } from 'react-redux';
 // import 'bootstrap/dist/css/bootstrap.min.css';
@@ -19,26 +19,41 @@ import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PrivateRoute from './components/PrivateRoute';
+import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<App />}>
-      <Route index={true} path='/' element={<HomeScreen />} />
-      <Route path='/product/:id' element={<ProductScreen />} />
-      <Route path='/cart' element={<CartScreen />} />
-      <Route path='/login' element={<LoginScreen />} />
-      <Route path='/register' element={<RegisterScreen />} />
-    </Route>
-  )
+    createRoutesFromElements(
+        <Route path='/' element={<App />}>
+            <Route index={true} path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+            <Route path='/cart' element={<CartScreen />} />
+            <Route path='/login' element={<LoginScreen />} />
+            <Route path='/register' element={<RegisterScreen />} />
+
+            {/* Registered Users */}
+            <Route path='' element={<PrivateRoute />}>
+                <Route path='/shipping' element={<ShippingScreen />} />
+                <Route path='/payment' element={<PaymentScreen />} />
+                <Route path='/placeorder' element={<PlaceOrderScreen />} />
+                <Route path='/order/:id' element={<OrderScreen />} />
+            </Route>
+        </Route>
+    )
 )
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-      <Provider store={store}>
-          <RouterProvider router={router} />
-      </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <PayPalScriptProvider deferLoading={ true }>
+                <RouterProvider router={router} />
+            </PayPalScriptProvider>      
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
