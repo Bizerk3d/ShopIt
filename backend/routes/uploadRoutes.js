8518f0ad695916fa8ec312fb00573d5e6a16f8ac
Brennan Reed <brtreed3@gmail.com>
2023-07-18 22:33:10 -0400

    Finish functionality for products, start on users

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -4,13 +4,35 @@ import multer from 'multer';
 
 const router = express.Router();
 
-const storagte = multer.diskStorage({
+const storage = multer.diskStorage({
     destination(req, file, cb) {
         cb(null, 'uploads/'); // null is for error
     },
     filename(req, file, cb) {
-        cb(null, `${file.fieldname}-${Data.now()}${path.extname(file.originalname)}`);
+        cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
     }
 });
+
+function checkFileType(file, cb) {
+    const filetypes = /jpg|jpeg|png/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+    if (extname && mimetype) {
+        return cb(null, true);
+    } else {
+        cb('Images only!');
+    }
+}
+
+const upload = multer({
+    storage,
+});
+
+router.post('/', upload.single('image'), (req, res) => {
+    res.send({
+        message: 'Image Uploaded',
+        image: `/${req.file.path}`,
+    });
+ });
 
 export default router;
--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -4,21 +4,36 @@ import { FaEdit, FaTrash } from 'react-icons/fa';
 import { toast } from 'react-toastify'
 import Message from '../../components/Message';
 import Loader from '../../components/Loader';
-import { useGetProductsQuery, useCreateProductMutation } from '../../slices/productsApiSlice';
+import { 
+    useGetProductsQuery,
+    useCreateProductMutation,
+    useDeleteProductMutation
+ } from '../../slices/productsApiSlice';
 
 const ProductListScreen = () => {
     const { data: products, isLoading, error, refetch } = useGetProductsQuery();
     
     const [createProduct, { isLoading: loadingCreate }] = useCreateProductMutation();
 
-    const deleteHandler = (id) => {
-        console.log('delete', id)
+    const [deleteProduct, { isLoading: loadingDelete }] = useDeleteProductMutation();
+
+    const deleteHandler = async (id) => {
+        if (window.confirm('Are you sure?')) {
+            try {
+                await deleteProduct(id);
+                toast.success('Product deleted');
+                refetch();
+            } catch (err) {
+                toast.error(err?.data?.message || err.error);
+            }
+        }
     };
 
     const createProductHandler = async () => {
         if (window.confirm('Are you sure you want to create a new product?')) {
             try {
                 await createProduct();
+                toast.success('Product added');
                 refetch();
             } catch (err) {
                 toast.error(err?.data?.message || err.error);
@@ -39,6 +54,7 @@ const ProductListScreen = () => {
         </Row>
 
         {loadingCreate && <Loader />}
+        {loadingDelete && <Loader />}
 
         { isLoading ? <Loader /> : error ? <Message variant='danger'>
             {error}</Message> : (
